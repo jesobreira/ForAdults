ba6af374f3bba060306a9a498803c2732b14101e
Reginaldo Junior <76regi@gmail.com>
2015-06-21 00:41:40 -0300

    listagem de videos

--- www/js/app.js
+++ www/js/app.js
@@ -1,22 +1,5 @@
 var app = angular.module('app', ['ngRoute']);
 
-function categoriesFunction($scope, $http) {
-  var url = 'http://cors-server.getup.io/url/api.redtube.com/?data=redtube.Categories.getCategoriesList&output=json';
-  var method = 'GET';
-  $http({
-    url: url,
-    method: method
-  }).
-  success(function(data){
-      $scope.categories = data;
-  }).
-  error(function(err){
-    console.log('Erro: ', err);
-  })
-
-  return $scope;
-};
-
 app.config(function($routeProvider, $locationProvider) {
  
    $routeProvider
@@ -35,20 +18,3 @@ app.config(function($routeProvider, $locationProvider) {
    .otherwise ({ redirectTo: '/' });
 });
     
-
-function CategoryController ($scope, $http) {
-   $scope.reverse = false;
-   $scope.predicate = 'name';
-
-   $scope.ordenar = function () {
-      $scope.predicate = 'price';
-      $scope.reverse = !$scope.reverse
-   }
-
-   var categories = categoriesFunction($scope, $http);
-
-   $scope.categories = $scope.categories;
-};
-
-CategoryController['$inject'] = ['$scope', '$http'];
-
